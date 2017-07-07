d42da2575d32aa349f6caf5fbe25125ad14feea0
Golden_Chow <570384231@qq.com>
2017-07-07 21:09:38 +0800

    初始项目搭建

---
+++ src/main/java/com/film/dao/FilmMapper.java
@@ -0,0 +1,9 @@
+package com.film.dao;
+
+/**
+ * Created by 曹金洲.
+ * 创建的时间：2017/7/7.
+ * 作用：
+ */
+public class FilmMapper {
+}
---
+++ src/main/java/com/film/interceptor/LoginInterceptor.java
@@ -0,0 +1,48 @@
+package com.film.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class LoginInterceptor implements HandlerInterceptor {
+
+	/** Handler拦截器方法执行完 */
+	@Override
+	public void afterCompletion(HttpServletRequest request,
+			HttpServletResponse response, Object object, Exception exception)
+			throws Exception {
+		
+	}
+
+	/** 进入Handler拦截器方法 */
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response,
+			Object object, ModelAndView modelAndView) throws Exception {
+
+	}
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+			Object object) throws Exception {
+		// 获取URL
+		String url = request.getRequestURI();
+		// 判断输入地址的URL，如果包含login，就放行
+		if(url.indexOf("loginUI") > 0 || url.indexOf("registUI") > 0){
+			return true;
+		}
+		
+		// 然后再判断session是否存在这个对象
+		HttpSession session = request.getSession();
+		if(session.getAttribute("user") != null){
+			return true;
+		}
+		
+		// 如果还执行到这里就代表条件还不满足，所以需要进行跳转，跳转到登录页面
+		request.getRequestDispatcher("/WEB-INF/jsp/login.jsp").forward(request, response);
+		
+		return false;
+	}
+}
